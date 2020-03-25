b81f076cbd65d375183b256e3cd97e30d58ddcc8
liangjiehai <850177158@qq.com>
2020-03-25 08:07:15 +0800

    t

--- src/main/java/com/example/mail/MailApplication.java
+++ src/main/java/com/example/mail/MailApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MailApplication {
 
 	public static void main(String[] args) {
+		//dev
 		SpringApplication.run(MailApplication.class, args);
 	}
 
